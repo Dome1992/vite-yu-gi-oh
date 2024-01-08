e7767339e058968e7bf164cc67cc64b93c4957cb
Dome1992 <forlanodomenico92@mail.com>
2024-01-08 16:13:04 +0100

    problemi AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+
+</script>
+
+<template>
+    <header class="container">
+
+        <h1 class ="text-center my-5">Titolo Header</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    font-weight: 700;
+}
+</style>
